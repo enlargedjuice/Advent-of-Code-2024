821d2852bd64ab59c55e598cb7bd270338cf0df2
enlargedjuice <135272041+enlargedjuice@users.noreply.github.com>
2026-09-01 12:40:05 -0400

    Update day7.py
    figured out the bug

--- Day 7/day7.py
+++ Day 7/day7.py
@@ -5,12 +5,15 @@ with open('Day 7/puzinp.txt', 'r') as file:
 
 problems = samp_inp.splitlines()
 
-better_problems = {}
+answers = []
+numbers = []
 #putting everything into a nice dictionary! finally get to use one :)
 for item in problems:
     refined = item.split(":")
     refined[1] = refined[1].split()
-    better_problems[refined[0]] = refined[1]
+    answers.append(refined[0])
+    numbers.append(refined[1])
+
 
 #ignore order of operations
 def order_ignorer(items):
@@ -24,13 +27,14 @@ def order_ignorer(items):
             result *= number
     return result
 
-def problem_checker(dict):
-    #first get all possible permutations of operations, a lot simpler than I originally thought!
+def problem_checker(ans,num):
+    #first get all possible combinations of operations, a lot simpler than I originally thought!
     correct_keys = []
     ops = "+*"
-    for key in dict.keys():
+    for key in ans:
+        ind = ans.index(key)
         #so many combinations!
-        op_comb = list(itertools.product(ops,repeat = len(dict[key])-1))
+        op_comb = list(itertools.product(ops,repeat = len(num[ind])-1))
         #quick way to check if everything is a power of 2
         #print(len(op_comb))
 
@@ -38,7 +42,7 @@ def problem_checker(dict):
         expression = []
         for operators in op_comb:
             exp = []
-            for number, operator in zip(dict[key], operators + (None,)):
+            for number, operator in zip(num[ind], operators + (None,)):
                     exp.append(number)
                     if operator is not None:
                         exp.append(operator)
@@ -51,8 +55,10 @@ def problem_checker(dict):
             #print(f"{exp} = {result}, {key}")
             fin_result = int(result)
             test_key = int(key)
+            #print(f"{exp} = {fin_result}, {key}")
             
             if fin_result == test_key:
+                
                 #print(f"{dict[key]} is correct!")
                 correct_keys.append(fin_result)
                 break
@@ -60,9 +66,7 @@ def problem_checker(dict):
                 continue
     return correct_keys
 
-
-
-print(f"Sum of correct values: {sum(problem_checker(better_problems))}")
-
-#i have no idea why this does not work
+print(f"Sum of correct values: {sum(problem_checker(answers,numbers))}")
+#DICTIONARY FUCKED ME
  
+
