b1d7820314bcfe964d65970c41eba563b62aad32
enlargedjuice <135272041+enlargedjuice@users.noreply.github.com>
2026-09-02 12:15:48 -0400

    Update day7.py

--- Day 7/day7.py
+++ Day 7/day7.py
@@ -25,12 +25,62 @@ def order_ignorer(items):
             result += number
         elif operator == '*':
             result *= number
+        elif operator == 'c':
+            newn = concatenator(result,number)
+            result = newn    
     return result
 
 def problem_checker(ans,num):
     #first get all possible combinations of operations, a lot simpler than I originally thought!
     correct_keys = []
     ops = "+*"
+    for key in ans:
+        ind = ans.index(key)
+        #so many combinations!
+        op_comb = list(itertools.product(ops,repeat = len(num[ind])-1))
+        #quick way to check if everything is a power of 2
+        #print(len(op_comb))
+        #combine numbers and operators, also formats it!
+        expression = []
+        for operators in op_comb:
+            exp = []
+            for number, operator in zip(num[ind], operators + (None,)):
+                    exp.append(number)
+                    if operator is not None:
+                        exp.append(operator)
+            expression.append(exp)
+
+        #check if they're right
+        for exp in expression:
+            result = order_ignorer(exp)
+            #print(f"{exp} = {result}, {key}")
+            fin_result = int(result)
+            test_key = int(key)
+
+            if fin_result == test_key:
+                
+                #print(f"{dict[key]} is correct!")
+                correct_keys.append(fin_result)
+                break
+            else:
+                continue
+    return correct_keys
+
+print(f"Sum of correct values: {sum(problem_checker(answers,numbers))}")
+#DICTIONARY FUCKED ME
+
+#part 2
+def concatenator(num1,num2):
+    numb = str(num1)
+    numby = str(num2)
+    conc = numb + numby
+    conc = int(conc)
+    return conc
+                          
+def problem_checker2(ans,num):
+    #first get all possible combinations of operations, a lot simpler than I originally thought!
+    correct_keys = []
+    ops = "+*c"
     for key in ans:
         ind = ans.index(key)
         #so many combinations!
@@ -47,16 +97,14 @@ def problem_checker(ans,num):
                     if operator is not None:
                         exp.append(operator)
             expression.append(exp)
-            
-   
+
         #check if they're right
         for exp in expression:
             result = order_ignorer(exp)
             #print(f"{exp} = {result}, {key}")
             fin_result = int(result)
             test_key = int(key)
-            #print(f"{exp} = {fin_result}, {key}")
-            
+
             if fin_result == test_key:
                 
                 #print(f"{dict[key]} is correct!")
@@ -66,7 +114,4 @@ def problem_checker(ans,num):
                 continue
     return correct_keys
 
-print(f"Sum of correct values: {sum(problem_checker(answers,numbers))}")
-#DICTIONARY FUCKED ME
- 
-
+print(f"Sum of correct values with concatenation: {sum(problem_checker2(answers,numbers))}")
